ca54b9e1a7c187fe47b656d08f6a4d230e2ea4b4
Mark Sweet <marks828@gmail.com>
2016-10-05 11:44:45 -0400

    populated index with pokemon names

--- app.rb
+++ app.rb
@@ -4,3 +4,12 @@ require "active_record"
 
 require_relative "db/connection"
 require_relative "models/pokemon"
+
+get '/' do
+  redirect "/pokemons"
+end
+
+get "/pokemons" do
+  @pokemon = Pokemon.all
+  erb :"pokemons/index"
+end
